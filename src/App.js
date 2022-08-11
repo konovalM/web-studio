336d3253d82dfdbff0cd1b79bbbb96d87b652b04
konovalM <konovalenko.ii4elka@mail.ru>
2022-08-11 18:39:20 +0300

    dinamic render of components w/ React.lazy()

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.scss';
-import MainPage from "./pages/MainPage";
-import ContextPage from "./pages/ContextPage";
-import React, {useEffect} from "react";
+import React, {useEffect, Suspense, lazy} from "react";
 import {
     Routes,
     Route,
 } from "react-router-dom";
-import SeoPage from "./pages/SeoPage";
-import MarketingPage from "./pages/MarketingPage";
-import SmmPage from "./pages/SmmPage";
-import ApplicationsPage from "./pages/ApplicationsPage";
-import ParserPage from "./pages/ParserPage";
-import SystemsPage from "./pages/SystemsPage";
 import AOS from "aos";
 import Layout from "./pages/Layout";
-import PolicyPage from "./pages/PolicyPage";
+const PolicyPage = lazy(() => import("./pages/PolicyPage"))
+const SystemsPage = lazy(() => import("./pages/SystemsPage"))
+const ParserPage = lazy(() => import("./pages/ParserPage"))
+const ApplicationsPage = lazy(() => import("./pages/ApplicationsPage"))
+const SmmPage = lazy(() => import("./pages/SmmPage"))
+const MarketingPage = lazy(() => import("./pages/MarketingPage"))
+const SeoPage = lazy(() => import("./pages/SeoPage"))
+const ContextPage = lazy(() => import("./pages/ContextPage"))
+const MainPage = lazy(() => import("./pages/MainPage"))
 
 function App() {
     useEffect(() => {
@@ -28,17 +28,19 @@ function App() {
     }, [])
     return (
         <Layout>
-            <Routes>
-                <Route path='/' element={<MainPage/>}/>
-                <Route path='context' element={<ContextPage/>}/>
-                <Route path='seo' element={<SeoPage/>}/>
-                <Route path='smm' element={<SmmPage/>}/>
-                <Route path='branding' element={<MarketingPage/>}/>
-                <Route path='apps' element={<ApplicationsPage/>}/>
-                <Route path='parser' element={<ParserPage/>}/>
-                <Route path='systems' element={<SystemsPage/>}/>
-                <Route path='policy' element={<PolicyPage/>}/>
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='/' element={<MainPage/>}/>
+                    <Route path='context' element={<ContextPage/>}/>
+                    <Route path='seo' element={<SeoPage/>}/>
+                    <Route path='smm' element={<SmmPage/>}/>
+                    <Route path='branding' element={<MarketingPage/>}/>
+                    <Route path='apps' element={<ApplicationsPage/>}/>
+                    <Route path='parser' element={<ParserPage/>}/>
+                    <Route path='systems' element={<SystemsPage/>}/>
+                    <Route path='policy' element={<PolicyPage/>}/>
+                </Routes>
+            </Suspense>
         </Layout>
     );
 }
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,10 +6,7 @@ import Services from "../components/Main/Services/Services";
 import Prices from "../components/prices/Prices";
 import About from "../components/Main/About/About";
 import Questions from "../components/Main/Questions";
-import Layout from "./Layout";
 import Faq from "../components/Main/Faq/Faq";
-import Header from "../components/Header/Header";
-import ScrollLock from "react-scrolllock";
 
 const MainPage = () => {
     return (
